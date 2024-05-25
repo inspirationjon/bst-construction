aed05d39a029bb777656804c04c77db6e0d28dcc
inspirationjon <ilkhomjon.interview@gmail.com>
2024-05-25 13:02:46 -0500

    Finish 3 3rd case

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ class BST {
     while (currentNode !== null) {
       if (value < currentNode.value) {
         currentNode = currentNode.left;
-      } else if (value < currentNode.value) {
+      } else if (value > currentNode.value) {
         currentNode = currentNode.right;
       } else {
         return true;
@@ -44,8 +44,61 @@ class BST {
     return false;
   }
 
-  remove(value) {
+  remove(value, parentNode = null) {
+    let currentNode = this;
+
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        // Case node needs to be removed
+
+        // Case 1: Node has two child
+        if (currentNode.left !== null && currentNode.right !== null) {
+          currentNode.value = currentNode.right.getMinValue();
+          currentNode.right.remove(currentNode.value, currentNode);
+        }
+
+        // Case 2: Node has one child and no child
+        else if (parentNode === null) {
+          // Root node
+          if (currentNode.left !== null) {
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          } else if (currentNode.right !== null) {
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+          } else {
+          }
+        }
+
+        // Case 3: Node is not root and has one child or no children
+        else if (parentNode.left === currentNode) {
+          parentNode.left =
+            currentNode.left !== null ? currentNode.left : currentNode.right;
+        } else if (parentNode.right === currentNode) {
+          parentNode.right =
+            currentNode.left !== null ? currentNode.left : currentNode.right;
+        }
+        break; // Exit the loop after the node is removed
+      }
+    }
     return this;
+  }
+
+  getMinValue() {
+    let currentNode = this;
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+
+    return currentNode.value;
   }
 }
 
