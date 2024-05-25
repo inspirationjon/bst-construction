9f3788f56504fefbb845b25d76b2b1ab5df26a8a
inspirationjon <ilkhomjon.interview@gmail.com>
2024-05-25 10:35:31 -0500

    Add insert function

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
 class BST {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    let currentNode = this;
+
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
     }
-  
-    insert(value) {
-      // Write your code here.
-      // Do not edit the return statement of this method.
-      return this;
-    }
-  
-    contains(value) {
-      // Write your code here.
-    }
-  
-    remove(value) {
-      // Write your code here.
-      // Do not edit the return statement of this method.
-      return this;
+    return this;
+  }
+
+  contains(value) {
+    let currentNode = this;
+
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left !== null) {
+          currentNode = currentNode.left;
+        } else {
+          return false;
+        }
+      } else if (value > currentNode.value) {
+        if (currentNode.right !== null) {
+          currentNode = currentNode.right;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
     }
   }
-  
-  // Do not edit the line below.
-  exports.BST = BST;
-  
+
+  remove(value) {
+    return this;
+  }
+}
+
+// Do not edit the line below.
+exports.BST = BST;
